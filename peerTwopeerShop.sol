cdd44f8e6fc0130c8cde85519891e2a29e5e1433
ELLIPTIC CURVE <121408579+EmbraArk@users.noreply.github.com>
2023-04-07 18:46:25 -0700

    1 lesson
    The first smart contract describes: 
    the function for paying for an item and withdrawing money from the balance of the smart contract to the address of the owner of the smart contract.

---
+++ peerTwopeerShop.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract peerTwopeerShop{
+    address public owner;
+    //  mapping имя: payments -- для хранения данных о покупателях
+    mapping (address => uint) public payments;
+
+    // инстанцирование класса при разворачивании ск в сети блокчейн
+    // конструктор: разворачивается единожды -- при разворачивании ск в сети блокчейн
+    constructor(){
+        // sender: return: адрес отправителя первого сигнала/ сообщения msg
+        owner = msg.sender;
+    }
+    
+    // оплата за покупку -- функция вызывается на этапе чекаута
+    // модификатор payable -- функция для приема денежных средств
+    // зачисление средств на счет ск
+    function payForItem() public payable {
+        // обращение к транзации через msg
+        payments[msg.sender] = msg.value;
+        }
+
+    function withdrawAll {
+        // для получение средств со счета ск создается временный адрес _to
+        // owner маркируется как payable и помещается в вдрес _to
+
+        address payable _to = payable (owner);
+        // сведения об адресе ск запрашиваются через ключ.слово this
+        address _thisContract = address(this);
+        _to.transfer(_thisContract.balance);
+    }
+}
